972d3ec0a3ddd20f85ed1a5a16a88e4ebdfb9778
rwenite <rwenite@gmail.com>
2017-06-05 05:47:33 -0400

    Fix tests

--- kwconfig/tests/test_manage.py
+++ kwconfig/tests/test_manage.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from os import remove
 from unittest import TestCase
 from tempfile import NamedTemporaryFile
 
@@ -7,63 +8,72 @@ import kwconfig
 
 class manageTest(TestCase):
 
-	def setUp(self):
-		self.manage = kwconfig.manage(NamedTemporaryFile().name, defaults={})
-		
-	def test_command(self):
-		manage = self.manage
-		with self.assertRaises(SystemExit):
-			manage.command(['-h'], i=0)
-		with self.assertRaises(SystemExit):
-			manage.command(['-v'], i=0)
-		with self.assertRaises(SystemExit):
-			manage.command(['-s', 'new_key1=newvalue1'], i=0)
-		with self.assertRaises(SystemExit):
-			manage.command(['-r', 'new_key1'], i=0)
-		with self.assertRaises(SystemExit):
-			manage.command(['-v'], i=0)
-		with self.assertRaises(SystemExit):
-			manage.command(['-d'], i=0)
-	
-	def test_reset(self):
-		manage = self.manage
-		manage.update({'key1': 'value1'})
-		manage.reset()
-		self.assertTrue({} == manage.read())
-		
-	def test_read(self):
-		manage = self.manage
-		manage.reset()
-		expected = {}
-		self.assertTrue(expected == manage.read())
-		
-	def test_update(self):
-		manage = self.manage
-		manage.reset()
-		expected = {'key1': 'value1', 'key2': 'value2'}
-		manage.update(expected)
-		self.assertTrue(expected == manage.read())
-		
-	def test_add(self):
-		manage = self.manage
-		manage.reset()
-		expected = {'key2': 'value2', 'key1': 'value1'}
-		manage.update({'key1': 'value1'})
-		added = manage.add({'key2': 'value2'})
-		self.assertTrue(expected == added)
-		
-	def test_overwrite(self):
-		manage = self.manage
-		manage.reset()
-		expected = {'key3': 'value3', 'key2': 'value2'}
-		manage.overwrite(expected)
-		self.assertTrue(expected == manage.read())
-		
-	def test_remove(self):
-		manage = self.manage
-		manage.reset()
-		expected = {'key2': 'value2'}
-		manage.update({'key1': 'value1', 'key2': 'value2'})
-		manage.remove('key1')
-		self.assertTrue(expected == manage.read())
-		
+  def setUp(self):
+    tempfile = NamedTemporaryFile()
+    self.in_path = tempfile.name
+    tempfile.close()
+    
+  def test_command(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    with self.assertRaises(SystemExit):
+      manage.command(['-h'], i=0)
+    with self.assertRaises(SystemExit):
+      manage.command(['-v'], i=0)
+    with self.assertRaises(SystemExit):
+      manage.command(['-s', 'new_key1=newvalue1'], i=0)
+    with self.assertRaises(SystemExit):
+      manage.command(['-r', 'new_key1'], i=0)
+    with self.assertRaises(SystemExit):
+      manage.command(['-v'], i=0)
+    with self.assertRaises(SystemExit):
+      manage.command(['-d'], i=0)
+    remove(self.in_path)
+  
+  def test_reset(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    manage.update({'key1': 'value1'})
+    manage.reset()
+    self.assertTrue({} == manage.read())
+    remove(self.in_path)
+    
+  def test_read(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    manage.reset()
+    expected = {}
+    self.assertTrue(expected == manage.read())
+    remove(self.in_path)
+    
+  def test_update(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    manage.reset()
+    expected = {'key1': 'value1', 'key2': 'value2'}
+    manage.update(expected)
+    self.assertTrue(expected == manage.read())
+    remove(self.in_path)
+    
+  def test_add(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    manage.reset()
+    expected = {'key2': 'value2', 'key1': 'value1'}
+    manage.update({'key1': 'value1'})
+    added = manage.add({'key2': 'value2'})
+    self.assertTrue(expected == added)
+    remove(self.in_path)
+    
+  def test_overwrite(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    manage.reset()
+    expected = {'key3': 'value3', 'key2': 'value2'}
+    manage.overwrite(expected)
+    self.assertTrue(expected == manage.read())
+    remove(self.in_path)
+    
+  def test_remove(self):
+    manage = kwconfig.manage(self.in_path, defaults={})
+    manage.reset()
+    expected = {'key2': 'value2'}
+    manage.update({'key1': 'value1', 'key2': 'value2'})
+    manage.remove('key1')
+    self.assertTrue(expected == manage.read())
+    remove(self.in_path)
+    
